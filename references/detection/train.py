638f31218ad670825d5315f2b3d2637bcdcea7bf
Lec_lizhaoliu <2524463910@qq.com>
2020-02-20 11:30:39 +0800

    refine reference.

--- references/detection/train.py
+++ references/detection/train.py
@@ -18,18 +18,17 @@ import time
 
 import torch
 import torch.utils.data
-from torch import nn
 import torchvision
 import torchvision.models.detection
 import torchvision.models.detection.mask_rcnn
 
-from coco_utils import get_coco, get_coco_kp
+from references.detection.coco_utils import get_coco, get_coco_kp
 
-from group_by_aspect_ratio import GroupedBatchSampler, create_aspect_ratio_groups
-from engine import train_one_epoch, evaluate
+from references.detection.group_by_aspect_ratio import GroupedBatchSampler, create_aspect_ratio_groups
+from references.detection.engine import train_one_epoch, evaluate
 
-import utils
-import transforms as T
+from references.detection import utils
+from references.detection import transforms as T
 
 
 def get_dataset(name, image_set, transform, data_path):
